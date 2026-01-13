c22873806de200db935c2ef9f6b43127a8eb5ab9
“isupov-zfort” <isupov@zfort.com>
2026-01-13 11:25:33 +0200

    Added basic styles for TopBar component

--- src/shared/layout/TopBar/components/TopBar.tsx
+++ src/shared/layout/TopBar/components/TopBar.tsx
@@ -8,31 +8,39 @@ import { CityList } from '@features/DeliveryRegion/components/CityList/CityList'
 import { Overlay } from '@shared/ui/Overlay/Overlay';
 import { CityButton } from '@features/DeliveryRegion/components/CityButton/CityButton';
 import { useCitySelectorState } from '@features/DeliveryRegion/hooks/useCitySelectorState';
+import { Icon } from '@shared/ui/Icon/Icon';
 
 export const TopBar = () => {
   const { city, isOpen, close, toggle, handleSelectCity } = useCitySelectorState();
 
   return (
-    <section className={'d-flex justify-between align-center'}>
+    <section className={`d-flex align-center ${styles.topBarSection}`}>
       <Container>
-        <div className={`d-flex ${styles.left}`}>
-          {isOpen && (
-            <Popup>
-              <Overlay onClose={close} />
-              <CityList city={city} onSelectCity={handleSelectCity} />
-            </Popup>
-          )}
-          <CityButton city={city} toggleOpen={toggle} variant={'mobile'} />
-          <div className={'d-flex'}>
-            {infoLinksConfig.map(({ id, label, href }) => (
-              <NavLink key={id} to={href}>
-                {label}
-              </NavLink>
-            ))}
+        <div className={`d-flex align-center justify-between ${styles.topBar}`}>
+          <div className={`d-flex ${styles.navigation}`}>
+            {isOpen && (
+              <>
+                <Overlay onClose={close} />
+                <Popup>
+                  <button className={`position-absolute ${styles.close}`} onClick={close}>
+                    <Icon name={'close'} />
+                  </button>
+                  <CityList city={city} onSelectCity={handleSelectCity} />
+                </Popup>
+              </>
+            )}
+            <CityButton city={city} toggleOpen={toggle} variant={'mobile'} />
+            <div className={'d-flex'}>
+              {infoLinksConfig.map(({ id, label, href }) => (
+                <NavLink key={id} to={href}>
+                  {label}
+                </NavLink>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={`d-flex ${styles.right}`}>
-          <div>ServiceBenefits</div>
+          <div className={`d-flex ${styles.benefits}`}>
+            <div>ServiceBenefits</div>
+          </div>
         </div>
       </Container>
     </section>
--- src/shared/ui/Popup/Popup.tsx
+++ src/shared/ui/Popup/Popup.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Popup.module.scss';
 
 interface PopupProps {
   children: React.ReactNode;
 }
 
 export const Popup = ({ children }: PopupProps) => {
-  return <div className={'popup'}>{children}</div>;
+  return <div className={`position-fixed w-50 p-8 ${styles.popup}`}>{children}</div>;
 };
