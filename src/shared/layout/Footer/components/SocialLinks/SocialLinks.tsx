90d32a63388c363c18825a30ceea559dcabc796e
“isupov-zfort” <isupov@zfort.com>
2026-01-15 12:18:18 +0200

    Added basic markup for SocialLinks and Subscription

--- src/shared/layout/Footer/components/SocialLinks/SocialLinks.tsx
+++ src/shared/layout/Footer/components/SocialLinks/SocialLinks.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
 import styles from './SocialLinks.module.scss';
+import { Icon } from '@shared/ui/Icon/Icon';
 
 export const SocialLinks = () => {
-  return <div className={`${styles.SocialLinks}`}>SocialLinks</div>;
+  return (
+    <nav className={`${styles.SocialLinks}`}>
+      <ul className="social-links">
+        <li>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <Icon name={'twitter'} />
+          </a>
+        </li>
+        <li>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <Icon name={'facebook'} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitch.tv" target="_blank" rel="noopener noreferrer">
+            <Icon name={'twitch'} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+            <Icon name={'youtube'} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <Icon name={'instagram'} />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
--- src/shared/layout/Footer/components/Subscription/Subscription.tsx
+++ src/shared/layout/Footer/components/Subscription/Subscription.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import styles from './Subscription.module.scss';
+import { Icon } from '@shared/ui/Icon/Icon';
 
 export const Subscription = () => {
-  return <div className={`${styles.subscription}`}>Subscription</div>;
+  return (
+    <div className={`${styles.subscription}`}>
+      <Icon name={'letter'} />
+      <span>
+        Подпишись на рассылку и получи <span className={'accent'}>-10%</span> скидку
+      </span>
+      <Icon name={'push'} />
+    </div>
+  );
 };
