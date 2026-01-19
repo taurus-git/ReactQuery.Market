3784be1c8447ae7e985998b8e394236e18b1c76b
“isupov-zfort” <isupov@zfort.com>
2026-01-19 15:39:18 +0200

    Changed div tag to button tag in Subscripton componnent

--- src/shared/layout/Footer/components/Subscription/Subscription.tsx
+++ src/shared/layout/Footer/components/Subscription/Subscription.tsx
@@ -4,12 +4,12 @@ import { Icon } from '@shared/ui/Icon/Icon';
 
 export const Subscription = () => {
   return (
-    <div className={`p-6 fw-bold ${styles.subscription}`}>
+    <button className={`p-6 fw-bold ${styles.subscription}`}>
       <Icon size={20} className={`${styles.icon} ${styles.iconLetter}`} name={'letter'} />
       <span>
         Подпишись на рассылку и получи <span className={'accent'}>-10%</span> скидку
       </span>
       <Icon size={22} className={`${styles.icon} ${styles.iconPush}`} name={'push'} />
-    </div>
+    </button>
   );
 };
