cb62b2c743ec1c9275f36b4209a17f8b3079b7a7
“isupov-zfort” <isupov@zfort.com>
2026-01-22 10:32:47 +0200

    Added buttons in header menu

--- src/shared/layout/Header/components/MobileNavigation/MobileNavigation.tsx
+++ src/shared/layout/Header/components/MobileNavigation/MobileNavigation.tsx
@@ -5,6 +5,7 @@ import { CloseButton } from '@shared/ui/CloseButton/CloseButton';
 import { MobileMenu } from '@shared/layout/Header/components/MobileMenu/MobileMenu';
 import { CityButton } from '@features/DeliveryRegion/components/CityButton/CityButton';
 import { InfoLinks } from '@shared/ui/InfoLinks/InfoLinks';
+import { Link } from 'react-router-dom';
 
 interface MobileNavigationProps {
   closeMenu: () => void;
@@ -21,9 +22,17 @@ export const MobileNavigation = ({ closeMenu, toggleOpen, city }: MobileNavigati
       <div className={'navigation'}>
         <MobileMenu onClose={closeMenu} />
       </div>
-      <div className={`d-flex flex-column justify-center align-center ${styles.links}`}>
-        <CityButton city={city} toggleOpen={toggleOpen} variant={'mobile'} />
-        <InfoLinks />
+
+      <div className={`d-flex flex-column justify-center align-center ${styles.linksWrapper}`}>
+        <div className={`w-100 d-flex flex-column justify-center align-center ${styles.actions}`}>
+          <Link to="/">Личный кабинет</Link>
+          <button className="btn btn--size-md btn--font-md btn--dark">Войти</button>
+          <button className="btn btn--size-md btn--font-md btn--light">Зарегистрироваться</button>
+        </div>
+        <div className={`d-flex flex-column justify-center align-center ${styles.links}`}>
+          <CityButton city={city} toggleOpen={toggleOpen} variant={'mobile'} />
+          <InfoLinks />
+        </div>
       </div>
     </div>
   );
